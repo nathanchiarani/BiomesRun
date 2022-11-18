36f71d60b371753d41c92a34b6fa6e900768ea21
Simone Riva <simone.riva05@CPT.local>
2022-11-18 14:28:43 +0100

    progetto

---
+++ 5_Sito o applicativo/BiomesRun/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float moveMultiplier;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    int punteggio;
+
+    Rigidbody rb;
+
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+    public Image imgLives;
+
+    private int lives = 3;
+
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    private float _time;
+
+
+    public int updateLives(int currentLives)
+    {
+        imgLives.sprite = _liveSprites[currentLives];
+        Debug.Log(currentLives);
+        return currentLives;
+    }
+
+    //public void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "mostro")
+    //    {
+    //        print("lost");
+    //        lives--;
+    //        imgLives.sprite = _liveSprites[updateLives(lives)];
+    //    }
+    //    if (lives == 0)
+    //    {
+    //        Destroy(this.gameObject);
+    //        //SceneManager.LoadScene(<perdita>);
+    //    }
+
+    //    //if (collision.gameObject.tag == "gemma")
+    //    //{
+    //    //    StartCoroutine(DestroyBonus(collision.gameObject));
+    //    //}
+    //}
+
+    //private void OnCollisionStay(Collision collision)
+    //{
+
+    //    if (collision.gameObject.CompareTag("mostro"))
+    //    {
+    //        _time += Time.deltaTime;
+    //        if (_time >= 0.3f)
+    //        {
+    //            print("lost");
+    //            lives--;
+    //            imgLives.sprite = _liveSprites[updateLives(lives)];
+    //            _time = 0;
+    //        }
+    //        if (lives == 0)
+    //        {
+    //            Destroy(this.gameObject);
+    //            //SceneManager.LoadScene(<perdita>);
+    //        }
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "mostro")
+        {
+            //collision.transform.position = new Vector3(, 0, 0);
+            print("lost");
+            
+        }
+        if (lives == 0)
+        {
+            Destroy(this.gameObject);
+            //SceneManager.LoadScene(<perdita>);
+        }
+    }
+
+    IEnumerator DecrementLives(GameObject obj)
+    {
+        lives--;
+        imgLives.sprite = _liveSprites[updateLives(lives)];
+    } //Coroutine
+
+
+
+    //IEnumerator DestroyBonus(GameObject obj) //Coroutine
+    //{
+    //    if (obj.gameObject.tag == "gemma")
+    //    {
+    //        obj.gameObject.GetComponent<BoxCollider>().enabled = false;
+    //        yield return new WaitForSeconds(0.25f);
+    //        punteggio++;
+    //        GameObject.FindGameObjectWithTag("ValorePunteggio").GetComponent<Text>().text = punteggio.ToString();
+    //        Destroy(obj.gameObject);
+    //    }
+    //}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        _time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        //handle drag
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        //calcolo direzione di movimento
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.velocity = moveDirection.normalized * moveSpeed * moveMultiplier;
+    }
+
+    public void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
